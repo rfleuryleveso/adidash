0d1be242f42b8ce5cc70c9e63c36957df3b323aa
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-02 14:30:31 +0100

    Added an alert when user doesn't have a classgroup selected

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,14 @@
 
 @section('title', 'Page Title')
 
+@section('page_name', 'Accueil')
+
 @section('content')
+    @if(Auth::user()->classgroup()->first()->id === 1)
+    <div class="notification is-warning">
+        Bonjour {{Auth::user()->fullName}}, <br/>
+        Vous n'avez pas encore choisi votre classe, veuillez la sélectionner dans votre profil
+      </div>
+    @endif
     <p>This is my body content.</p>
 @endsection
