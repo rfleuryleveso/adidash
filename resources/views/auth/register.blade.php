60df713299d31925f72762d6b399ad0d97cb8b03
LouisLecouturier <louis.lecout@gmail.com>
2020-12-04 17:15:31 +0100

    Modifs authentification
    Il manque plus que les animations en javascript sur les inputs..

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,9 +22,7 @@
                     </div>
                 </div>
 
-                @error('first_name')
-                    {{ $message }}
-                @enderror
+
 
                 <div>
                     <label for="last_name">Nom :</label>
@@ -35,9 +33,7 @@
                 </div>
             </div>
 
-            @error('last_name')
-                {{ $message }}
-            @enderror
+
             <label for="email">Email:</label>
             <div class="auth-form">
                 <i class="gg-mail"></i><input class="auth-field" type="text" id="email" name="email"
@@ -49,15 +45,29 @@
                 <i class="gg-lock"></i><input class="auth-field" type="password" id="password" name="password"
                     placeholder="Mot de passe">
             </div>
-            @error('password')
-                {{ $message }}
-            @enderror
 
             <label for="password_confirmation">Confirmer le mot de passe :</label>
             <div class="auth-form">
                 <i class="gg-lock"></i><input class="auth-field" type="password" id="password_confirmation"
                     name="password_confirmation" placeholder="Confirmer le mot de passe">
             </div>
+            @error('first_name')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+
+            @error('last_name')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+
+            @error('mail')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+
+            @error('password')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+
+
             <div class="form-button">
                 <button type="submit" class="submit-button">S'inscrire</button>
             </div>
