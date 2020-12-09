a21dfcd7f7befff372d972b8a51e1c83af674eda
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-09 12:34:37 +0100

    Style modification & error handling fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,54 +3,54 @@
 @section('title', 'Login')
 
 @section('content')
+    <div class="auth-form-box">
+        <h1 class="auth-title">Connexion :</h1>
+        <form action="/login" method="POST">
+            @csrf
 
-    <div class="auth-container">
+            <div>
+                <label for="email">Email:</label><br>
+                @if ($errors->has('email'))
+                    <div class="auth-form-input red-input">
+                    @else
+                        <div class="auth-form-input">
+                @endif
 
-        <div class="auth-form-box">
-            <h1 class="auth-title">Connexion :</h1>
+                <i class="gg-mail"></i><input class="auth-field" type="text" id="email" name="email"
+                    placeholder="prenom.nom@yncrea.fr">
+                @error('email')
+                    {{ $message }}
+                @enderror
+            </div>
+            <div>
 
-            <form action="/login" method="POST">
-                @csrf
+                <label for="password">Mot de passe:</label><br>
+                @if ($errors->any())
+                    <div class="auth-form-input red-input">
+                    @else
+                        <div class="auth-form-input">
+                @endif
 
-                <div>
-                    <label for="email">Email:</label><br>
-                    @if ($errors->any())
-                        <div class="auth-form-input red-input">
-                        @else
-                            <div class="auth-form-input">
-                    @endif
+                <i class="gg-lock"></i><input class="auth-field" type="password" id="password" name="password"
+                    placeholder="Mot de passe">
 
-                    <i class="gg-mail"></i><input class="auth-field" type="text" id="email" name="email"
-                        placeholder="prenom.nom@yncrea.fr">
-                </div>
-                <div>
-
-                    <label for="password">Mot de passe:</label><br>
-                    @if ($errors->any())
-                        <div class="auth-form-input red-input">
-                        @else
-                            <div class="auth-form-input">
-                    @endif
-
-                    <i class="gg-lock"></i><input class="auth-field" type="password" id="password" name="password"
-                        placeholder="Mot de passe">
-
-                    @error('password')
-                        {{ $message }}
-                    @enderror
-                </div>
+                @error('password')
+                    {{ $message }}
+                @enderror
+            </div>
 
 
-                <div class="remember">
-                    <label for="remember">Rester connecté ?</label>
-                    <input type="checkbox" id="remember" name="remember">
-                </div>
-                <div class="form-bottom">
-                    <button type="submit" class="submit-button">Se connecter</button>
-                    <p>Pas de compte ? <a href="#">En créer un ici</a></p>
-                </div>
-            </form>
-        </div>
+            <div class="remember">
+                <label for="remember">Rester connecté ?</label>
+                <input type="checkbox" id="remember" name="remember">
+            </div>
+            <div class="form-bottom">
+                <button type="submit" class="submit-button">Se connecter</button>
+                <p class="switch-button">Pas encore inscrit ? <a href="{{ route('register') }}">Créer un compte</a>
+                </p>
+            </div>
+        </form>
     </div>
+
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
         <div class="auth-form-box">
             <h1 class="auth-title">S'inscrire :</h1>
 
-            <form action="/register" method="POST">
+            <form action="{{ route('register') }}" method="POST">
                 @csrf
                 <div class="name-row">
                     <div>
@@ -18,6 +18,9 @@
                             <i class="gg-profile"></i><input type="text" id="first_name" class="register-auth-field"
                                 name="first_name" placeholder="Prénom">
                         </div>
+                        @error('first_name')
+                            <p class="error-message">{{ $message }}</p>
+                        @enderror
                     </div>
 
 
@@ -28,6 +31,9 @@
                             <i class="gg-profile"></i><input type="text" id="last_name" class="register-auth-field"
                                 name="last_name" placeholder="Nom">
                         </div>
+                        @error('last_name')
+                            <p class="error-message">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
 
@@ -37,39 +43,34 @@
                     <i class="gg-mail"></i><input class="auth-field" type="text" id="email" name="email"
                         placeholder="prenom.nom@yncrea.fr">
                 </div>
+                @error('email')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
 
                 <label for="password">Mot de passe:</label>
                 <div class="auth-form-input">
                     <i class="gg-lock"></i><input class="auth-field" type="password" id="password" name="password"
                         placeholder="Mot de passe">
                 </div>
+                @error('password')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
 
                 <label for="password_confirmation">Confirmer le mot de passe :</label>
                 <div class="auth-form-input">
                     <i class="gg-lock"></i><input class="auth-field" type="password" id="password_confirmation"
                         name="password_confirmation" placeholder="Confirmer le mot de passe">
                 </div>
-
-                @error('first_name')
+                @error('password_confirmation')
                     <p class="error-message">{{ $message }}</p>
                 @enderror
 
-                @error('last_name')
-                    <p class="error-message">{{ $message }}</p>
-                @enderror
-
-                @error('mail')
-                    <p class="error-message">{{ $message }}</p>
-                @enderror
-
-                @error('password')
-                    <p class="error-message">{{ $message }}</p>
-                @enderror
 
 
                 <div class="form-bottom">
                     <button type="submit" class="submit-button">S'inscrire</button>
-                    <a class="submit-button switch-button">Se connecter</a>
+                    <p class="switch-button">Déja inscrit ? <a href="{{ route('login') }}">Se connecter</a>
+                    </p>
                 </div>
 
             </form>
