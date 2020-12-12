fe3783c4484a07721430fe19847cbf0d079c7e8c
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-12 18:58:17 +0100

    Added comments & group helpers to User class

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,21 +50,64 @@ class User extends Authenticatable
         return "{$this->first_name} {$this->last_name}";
     }
 
+    
+    /**
+     * Get the user's groups
+     *
+     * @return [Group]
+     */
     public function groups()
     {
         return $this->belongsToMany(Group::class);
     }
 
+    /**
+     * Get the user's tasks
+     * 
+     * @return [Task]
+     */
     public function tasks()
     {
         return $this->belongsToMany('App\Models\Task')->using('App\Models\TaskRole');
     }
 
+    /**
+     * Helper functions for groups
+     */
+    
+    /**
+     * Get the user's classgroup
+     * 
+     * @return Group
+     */
     public function getClassGroup() {
         return $this->groups()->where('is_class', true)->first();
     }
 
+    /**
+     * Check if the user has a class group
+     * 
+     * @return Boolean
+     */
     public function hasClassGroup() {
         return $this->getClassGroup() != null;
+    }
+
+    /**
+     * Get the user's comity group
+     * 
+     * @return Group
+     */
+    public function getComityGroup() {
+        return $this->groups()->where('rank', 2)->first();
+    }
+
+    /**
+     * Check if the user has a comity group
+     * 
+     * @return Boolean
+     */
+    public function hasComityGroup() {
+        return $this->getComityGroup() != null;
     }
 }
