a86d19445f10cd7cf0fe90859736f1feb61eb328
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-13 00:43:05 +0100

    Corrected typo

--- database/migrations/2020_11_24_142509_create_task_users_table.php
+++ database/migrations/2020_11_24_142509_create_task_users_table.php
@@ -13,7 +13,7 @@ class CreateTaskUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('task_users', function (Blueprint $table) {
+        Schema::create('task_user', function (Blueprint $table) {
             $table->id();
             $table->integer("user_id");
             $table->integer("task_id");
