78f150c5725ea1963ab3f4122b34ed3fe626886e
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-13 00:39:42 +0100

    Added weekly project tasks

--- app/Http/Controllers/ProjectAdmin/ProjectAdminController.php
+++ app/Http/Controllers/ProjectAdmin/ProjectAdminController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers\ProjectAdmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Project;
+
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\View;
+
+use \Carbon\Carbon;
 
 class ProjectAdminController extends Controller
 {
@@ -15,13 +21,26 @@ class ProjectAdminController extends Controller
      *
      * @return void
      */
-    public function __construct(Request $request, Project $project)
+    public function __construct(Request $request)
     {
         
     }
 
     public function home(Project $project)
     {
-        return view('project-admin.index', ['project'=>$project]);
+
+        $week_start = \Carbon\Carbon::now()->previous(Carbon::MONDAY);
+        $week_end = $week_start->copy()->next(Carbon::SUNDAY); // Generate bounds
+
+        $week_tasks = $project->tasks()->where([
+            ['ends_at', '>=', $week_start],
+            ['ends_at', '<=', $week_end]
+        ])->orWhere([
+            ['ended_at', '>=', $week_start],
+            ['ended_at', '<=', $week_end]
+        ])->get();
+        // Check for tasks within the bounds
+
+        return view('project-admin.index', ['project'=>$project, 'week_tasks' => $week_tasks]);
     }
 }
