e9c59284926769139b377b9eba70af71fd611d02
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-12 22:47:51 +0100

    Added bulma steps package

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,8 @@ const mix = require("laravel-mix");
 
 mix.sass("resources/sass/app.scss", "public/dist/css")
     .sass("node_modules/bulma/bulma.sass", "public/dist/css")
+    .css("node_modules/bulma-steps/dist/css/bulma-steps.css", "public/dist/css")
+    .js("node_modules/bulma-steps/dist/js/bulma-steps.min.js", "public/dist/js/bulma-steps.min.js")
     .styles(
         ["node_modules/normalize.css/normalize.css"],
         "public/dist/css/normalize.css"
