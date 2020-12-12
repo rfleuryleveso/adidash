1f33b90e1a252df6ba8df8a6c15ab73b622e6125
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-13 00:40:01 +0100

    Implemented reusable status tags

---
+++ app/View/Components/TaskStatus.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class TaskStatus extends Component
+{
+    /**
+     * The status.
+     *
+     * @var string
+     */
+    public $status;
+
+    /**
+     * Create a new component instance.
+     * @param  string  $status
+     * @return void
+     */
+    public function __construct($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        $STATUS_COLOR = [
+            "WAITING_FOR_PARENT_TASK" => "is-warning",
+            "WAITING" => "is-light",
+            "STARTED" => "is-info",
+            "FINISHED" => "is-success",
+            "CANCELLED" => "is-black"
+        ];
+        $STATUS_FRIENDLY = [
+            "WAITING_FOR_PARENT_TASK" => "En attente de la tâche parente",
+            "WAITING" => "En attente",
+            "STARTED" => "Commencé",
+            "FINISHED" => "Fini",
+            "CANCELLED" => "Annulé"
+        ];
+        return view('components.task-status', ['status' => $this->status, 'color' => $STATUS_COLOR[$this->status], 'friendly' => $STATUS_FRIENDLY[$this->status]] );
+    }
+}
