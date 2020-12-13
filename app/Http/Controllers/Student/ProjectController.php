772e3b932725c64ce5162658fc7cc13e7bad573f
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-13 11:39:51 +0100

    Added working project list

--- app/Http/Controllers/Student/ProjectController.php
+++ app/Http/Controllers/Student/ProjectController.php
@@ -15,28 +15,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view("student.projects.index");
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $projects = Project::all();
+        return view("student.projects.index", ["projects" => $projects]);
     }
 
     /**
@@ -47,40 +27,6 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Project $project)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Project $project)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Project $project)
-    {
-        //
+        return view("student.projects.project", ["project" => $project]);
     }
 }
--- resources/views/student/projects/index.blade.php
+++ resources/views/student/projects/index.blade.php
@@ -5,8 +5,23 @@
 @section('page_name', 'Projets')
 
 @section('content')
-    <p>This is my body dadazd.</p>
-    <div class="blabla">
-        dqsdqs
+    <div class="columns">
+        @foreach ($projects as $project)
+            <div class="column is-one-quarter">
+                <a href="{{ route('student.project', ['project'=> $project->id]) }}">
+                <div class="box @if($project->background_image) project-has-background has-text-white @endif" @if($project->background_image) style="background-image: url('{{ $project->background_image }}')" @endif>
+                    <p class="is-size-4">{{ $project->name }}</p>
+                    <p class="is-size-7 @if($project->background_image) has-text-white @else has-text-grey-dark @endif">
+                        {{ !empty($project->intro) ? $project->intro : Str::limit($project->description, 32) }}</p>
+                    <p class="is-size-7 @if($project->background_image) has-text-white @else has-text-grey-dark @endif mt-3"><i class="far fa-user"></i> {{ $project->members()->wherePivot('relation_type', 3)->get()->map(function ($user) {
+                                return $user->fullName;
+                            })->join(", ") }}</p>
+                    @if($project->ends_at)
+                        <p class="is-size-6 @if($project->background_image) has-text-white @else has-text-grey-dark @endif"><i class="far fa-calendar"></i> {{ $project->ends_at->toDateString() }}</p>
+                    @endif
+                </div>
+                </a>
+            </div>
+        @endforeach
     </div>
 @endsection
