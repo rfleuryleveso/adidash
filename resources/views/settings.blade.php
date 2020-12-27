142d24adb5d68254f275846353575ae343e2b258
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-27 22:37:02 +0100

    Added the ability to change classgroup, and a password check.

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -14,7 +14,7 @@
                     <input class="input" type="text" name="first_name" value="{{ Auth::user()->first_name }}"
                         placeholder="{{ Auth::user()->first_name }}">
                 </div>
-                @error("first_name") <p class="help is-danger">{{ $message }}</p> @enderror
+                @error('first_name') <p class="help is-danger">{{ $message }}</p> @enderror
             </div>
             <div class="field">
                 <label class="label">Nom</label>
@@ -22,7 +22,7 @@
                     <input class="input" type="text" name="last_name" value="{{ Auth::user()->last_name }}"
                         placeholder="{{ Auth::user()->last_name }}">
                 </div>
-                @error("last_name") <p class="help is-danger">{{ $message }}</p> @enderror
+                @error('last_name') <p class="help is-danger">{{ $message }}</p> @enderror
             </div>
             <div class="field">
                 <label class="label">Email</label>
@@ -30,7 +30,28 @@
                     <input class="input" type="text" name="email" value="{{ Auth::user()->email }}"
                         placeholder="{{ Auth::user()->email }}">
                 </div>
-                @error("email") <p class="help is-danger">{{ $message }}</p> @enderror
+                @error('email') <p class="help is-danger">{{ $message }}</p> @enderror
+            </div>
+            <div class="field">
+                <label class="label">Classe</label>
+                <div class="select">
+                    <select name="class_group">
+                        @foreach ($availableGroups as $group)
+                            <option value="{{ $group->id }}" @if (Auth::user()->hasClassGroup() && $group->id == Auth::user()->getClassGroups()->first()->id)
+                                selected="selected"
+                        @endif
+                        >{{ $group->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                @error('class_group') <p class="help is-danger">{{ $message }}</p> @enderror
+            </div>
+            <div class="field">
+                <label class="label">Mot de passe actuel</label>
+                <div class="control">
+                    <input class="input" type="password" name="password" required placeholder="Votre mot de passe.">
+                </div>
+                @error('password') <p class="help is-danger">{{ $message }}</p> @enderror
             </div>
             <div class="field is-grouped">
                 <div class="control">
