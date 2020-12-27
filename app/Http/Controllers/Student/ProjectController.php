2ecce885ef17b8c93c1fb0e85e8c4f145d3b7716
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-27 22:39:53 +0100

    Enhanced project controller fetching of projects

--- app/Http/Controllers/Student/ProjectController.php
+++ app/Http/Controllers/Student/ProjectController.php
@@ -4,26 +4,30 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use Auth;
 
-class ProjectController extends Controller {
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index() {
-		$projects = Project::all();
-		return view("student.projects.index", ["projects" => $projects]);
-	}
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Auth::user()->groupProjects()->where('status', 'STARTED')->get();
+        return view("student.projects.index", ["projects" => $projects]);
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  \App\Models\Project  $project
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show(Project $project) {
-		$available_tasks = $project->tasks()->whereIn('status', ['WAITING', 'STARTED'])->get();
-		return view("student.projects.project", ["project" => $project, "available_tasks" => $available_tasks]);
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        $available_tasks = $project->tasks()->whereIn('status', ['WAITING', 'STARTED'])->get();
+        return view("student.projects.project", ["project" => $project, "available_tasks" => $available_tasks]);
+    }
 }
