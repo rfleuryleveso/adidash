ff671159d79e36fae9bbe9e04043de7dd252f711
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-27 22:31:36 +0100

    Created form requests for committee and project chief

---
+++ app/Http/Requests/CommitteeCreateProjectRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommitteeCreateProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'project-chiefs' => 'required|array|size:1',
+            'project-chiefs.*' => 'exists:users,id', 
+            'groups' => 'required|array|size:1',
+            'groups.*' => 'exists:groups,id', 
+        ];
+    }
+}
