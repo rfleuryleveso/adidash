fa5afc949d4531814621eabfd70c07af0072c1dd
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-27 22:38:58 +0100

    Added userpolicy, committee gate, administrator check

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,26 +4,40 @@ namespace App\Providers;
 
 use App\Models\Project;
 use App\Policies\ProjectPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
-class AuthServiceProvider extends ServiceProvider {
-	/**
-	 * The policy mappings for the application.
-	 *
-	 * @var array
-	 */
-	protected $policies = [
-		Project::class => ProjectPolicy::class,
-	];
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Project::class => ProjectPolicy::class,
+        User::class, UserPolicy::class
+    ];
 
-	/**
-	 * Register any authentication / authorization services.
-	 *
-	 * @return void
-	 */
-	public function boot() {
-		$this->registerPolicies();
-
-		//
-	}
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+        
+        Gate::before(function ($user, $ability) {
+            if ($user->isAdministrator()) {
+                return true;
+            }
+        });
+        
+        Gate::define('access-committee', function (User $user) {
+            return $user->hasCommitteeGroup();
+        });
+    }
 }
