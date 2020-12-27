ded664ea3dd64d480b88cd8af3574ee0c57ec1e8
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-27 22:34:24 +0100

    Added a tags list (api only)

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use Auth;
+use App\Models\Tag;
+use App\Http\Resources\Tag as TagResource;
 
-class HomeController extends Controller {
-	public function home() {
-		return view('student.home.home');
-	}
+class HomeController extends Controller
+{
+    public function home()
+    {
+        return view('student.home.home');
+    }
 
-	public function logout() {
-		Auth::Logout();
-		return redirect('/');
-	}
+    public function logout()
+    {
+        Auth::Logout();
+        return redirect('/');
+    }
 
+    public function tags()
+    {
+        $tags = Tag::all();
+        return TagResource::collection($tags);
+    }
 }
