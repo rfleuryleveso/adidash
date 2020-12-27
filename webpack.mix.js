b0dd3056e6c5fb4a8fe2ade35eb46ef759986bd4
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-27 22:40:17 +0100

    Added bulma calendar dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,14 +10,24 @@ const mix = require("laravel-mix");
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    output: {
+        library: ['bulmaCalendar']
+    }
+});
 
 mix.sass("resources/sass/app.scss", "public/dist/css")
     .sass("node_modules/bulma/bulma.sass", "public/dist/css")
     .css("node_modules/bulma-steps/dist/css/bulma-steps.css", "public/dist/css")
-    .js("node_modules/bulma-steps/dist/js/bulma-steps.min.js", "public/dist/js/bulma-steps.min.js")
+    .styles(["node_modules/bulma-calendar/dist/css/bulma-calendar.min.css"], "public/dist/css/bulma-calendar.min.css")
+    .styles(['node_modules/choices.js/public/assets/styles/choices.min.css'], 'public/dist/css/choices.min.css')
     .styles(
         ["node_modules/normalize.css/normalize.css"],
         "public/dist/css/normalize.css"
     )
+    .js("node_modules/bulma-steps/dist/js/bulma-steps.min.js", "public/dist/js/bulma-steps.min.js")
+    
+    .js("resources/js/project-admin/create-task.js", "public/dist/js/create-task.js")
+    .js('node_modules/choices.js/public/assets/scripts/choices.min.js', 'public/dist/js/choices.min.js')
     .js('resources/js/app.js', 'public/dist/js/app.js')
     .js('resources/js/calendar/index.js', 'public/dist/js/calendar.js')
