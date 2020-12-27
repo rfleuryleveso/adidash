b9b76092db23eabdb3a6d058f543d6e47c0d6e2a
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2020-12-27 22:34:44 +0100

    Finished the settings controller.

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Group;
+use Illuminate\Validation\Rule;
+use App\Models\GroupUser;
 
 class SettingsController extends Controller
 {
@@ -15,7 +18,8 @@ class SettingsController extends Controller
      */
     public function home()
     {
-        return view('settings');
+        $availableGroups = Group::where("is_class", true)->get();
+        return view('settings', ['availableGroups' => $availableGroups]);
     }
 
     /**
@@ -26,14 +30,43 @@ class SettingsController extends Controller
      */
     public function update(Request $request)
     {
+        $availableGroups = Group::where("is_class", true)->get();
+        $availableGroupsIds = $availableGroups->map(function ($group) {
+            return $group->id;
+        })->all();
+
         $validated = $request->validate([
             'first_name' => ["required", "max:255"],
             'last_name' => ["required", "max:255"],
             'email' => ["required", "email"],
+            'password' => ["required", "password"],
+            'new_password' => ["filled"],
+            'class_group' => ["required", "integer", Rule::in($availableGroupsIds)]
         ]);
+        $forms = collect($validated);
+        
+        // Update
+        
         $user = Auth::user();
-        $user->fill($validated);
+        $user->fill($forms->only("first_name", "last_name", "email")->all());
         $user->save();
-        return redirect('settings')->with('success', 'Profile mis à jour avec succès');
+        
+        // Update class
+        if (Auth::user()->hasClassGroup()) {
+            $currentClassGroup = Auth::user()->getClassGroups()->first();
+            $pivotModel = GroupUser::where([
+                "user_id" => Auth::id(),
+                "group_id" => $currentClassGroup->id
+            ])->first();
+            $pivotModel->delete();
+        }
+        
+        $newPivot = new GroupUser;
+
+        $newPivot->user_id = Auth::id();
+        $newPivot->group_id = $forms->get("class_group");
+        $newPivot->save();
+        
+        return redirect('settings')->with('success', 'Profil mis à jour avec succès');
     }
 }
