f3f2f0ba49c94c59b80835c290af63aea15884b0
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2021-04-01 11:08:25 +0200

    Improved home page

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\Deliverable;
 use Auth;
 use App\Models\Tag;
 use App\Http\Resources\Tag as TagResource;
@@ -11,7 +12,16 @@ class HomeController extends Controller
 {
     public function home()
     {
-        return view('student.home.home');
+        $tasks = Auth::user()->tasks();
+        $tasksCount = $tasks->count();
+        $waitingForDeliverable = $tasks->where('status', 'STARTED')
+            ->whereNotIn('tasks.id', function ($query) {
+                $query->select('deliverables.task_id')
+                    ->from(with(new Deliverable)->getTable());
+            })->count();
+        $projets = Auth::user()->projects;
+        $activeTasks = Auth::user()->tasks()->where('status', 'ACTIVE')->get();
+        return view('student.home.home', ['tasks' => $tasks, 'tasksCount' => $tasksCount, 'waitingForDeliverable' => $waitingForDeliverable, 'projects' => $projets, 'activeTasks' => $activeTasks]);
     }
 
     public function logout()
--- resources/views/student/home/home.blade.php
+++ resources/views/student/home/home.blade.php
@@ -9,7 +9,7 @@
     <main class="content">
         @if (!Auth::user()->hasClassGroup())
             <div class="notification is-warning">
-                Bonjour {{ Auth::user()->fullName }}, <br />
+                Bonjour {{ Auth::user()->fullName }}, <br/>
                 Vous n'avez pas encore choisi votre classe, veuillez la sélectionner dans votre profil
             </div>
         @endif
@@ -26,14 +26,14 @@
                                     <span>Etoiles : 4</span>
                                 </li>
                                 <li class="grid-item summary-tasks"><i class="fas fa-check-double"></i>
-                                    <span>Tâches en cours : 3</span>
+                                    <span>Tâches en cours : {{$tasksCount}}</span>
                                 </li>
                                 <li class="grid-item summary-works"><i class="fas fa-truck-loading"></i><span>En attente de
-                                        livrable : 1</span>
+                                        livrable : {{$waitingForDeliverable}}</span>
                                 </li>
                             </ul>
                         </section>
-                        <section>
+                        <!-- <section>
                             <h5>Evaluation :</h5>
                             <ul>
                                 <li class="grid-item summary-total-stars">
@@ -45,53 +45,56 @@
                                     <span>Note provisoire : 13.45 / 20</span>
                                 </li>
                             </ul>
-                        </section>
+                        </section> -->
                     </div>
                 </div>
             </section>
 
 
-
             <section class="worked-recently">
-                <h1 class="section-title">Travaillés récemment</h1>
+                <h1 class="section-title">Projets rejoints</h1>
 
-                <hr class="section-line" />
+                <hr class="section-line"/>
 
                 <div class="container">
-                    <article class="project-item">
-                        <div class="project-thumbnail">
-                            <span class="thumbnail-title">Valise Bombe</span>
-                            <span class="thumbnail-user">
+                    @foreach($projects as $project)
+                        <article class="project-item">
+                            <div class="project-thumbnail">
+                                <span class="thumbnail-title">{{$project->name}}</span>
+                                <span class="thumbnail-user">
                                 <i class="fas fa-crown"></i>
-                                John Doe</span>
-                        </div>
-                    </article>
+                                {{$project->admins()->first()->fullName}}</span>
+                            </div>
+                        </article>
+                    @endforeach
                 </div>
             </section>
 
             <section class="worked-recently">
-                <h1 class="section-title">Nouvelles tâches</h1>
+                <h1 class="section-title">Vos tâches en cours</h1>
 
-                <hr class="section-line" />
+                <hr class="section-line"/>
 
                 <div class="container">
-                    <article class="task-item">
-                        <div class="task-content">
-                            <h1 class="task-title">Coder la page des tâches</h1>
-                            <p class="task-description">Lorem, ipsum dolor sit.</p>
-                            <div class="task-info">
-                                <span>4h</span>
-                                <ul>
-                                    <li><i class="fas fa-star star"></i></li>
-                                    <li><i class="fas fa-star star"></i></li>
-                                </ul>
+                    @foreach($activeTasks as $activeTask)
+                        <article class="task-item">
+                            <div class="task-content">
+                                <h1 class="task-title">{{$activeTask->name}}</h1>
+                                <p class="task-description">Lorem, ipsum dolor sit.</p>
+                                <div class="task-info">
+                                    <span>4h</span>
+                                    <ul>
+                                        <li><i class="fas fa-star star"></i></li>
+                                        <li><i class="fas fa-star star"></i></li>
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                        <div class="task-project">
-                            <h1 class="task-project-title">Adidash</h1>
-                            <span class="project-leader"></span>
-                        </div>
-                    </article>
+                            <div class="task-project">
+                                <h1 class="task-project-title">Adidash</h1>
+                                <span class="project-leader"></span>
+                            </div>
+                        </article>
+                    @endforeach
                 </div>
             </section>
         </div>
