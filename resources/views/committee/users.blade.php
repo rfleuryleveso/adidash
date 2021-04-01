8756ac529eb331efabb32271464d1f957a4e0bef
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2021-04-01 11:08:56 +0200

    Fixed visual glitch

--- resources/views/committee/users.blade.php
+++ resources/views/committee/users.blade.php
@@ -8,23 +8,23 @@
     <div class="content-container commitee">
         <table class="table is-fullwidth">
             <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Nom</th>
-                    <th>Prénom</th>
-                    <th></th>
-                </tr>
+            <tr>
+                <th>#</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th></th>
+            </tr>
             </thead>
             <tbody>
-                @foreach ($users as $user)
-                    <tr>
-                        <th>{{ $user->id }}</th>
-                        <td>{{ $user->last_name }}</td>
-                        <td>{{ $user->first_name }}</td>
-                        <td><a href="{{ route('committee.user', ['user' => $user->id]) }}"><i class="far fa-eye" /></a>
-                        </td>
-                    </tr>
-                @endforeach
+            @foreach ($users as $user)
+                <tr>
+                    <th>{{ $user->id }}</th>
+                    <td>{{ $user->last_name }}</td>
+                    <td>{{ $user->first_name }}</td>
+                    <td><a href="{{ route('committee.user', ['user' => $user->id]) }}"><i class="far fa-eye"></i></a>
+                    </td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
