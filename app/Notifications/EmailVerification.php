a54cad9fa029c5adef2fb1619486327c19e9dee1
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2021-04-14 09:15:58 +0200

    Added forgotten mail verification notification

---
+++ app/Notifications/EmailVerification.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Auth\Notifications\VerifyEmail as VerifyEmailBase;
+
+class EmailVerification extends VerifyEmailBase
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Merci d\'avoir créée un compte sur Adidash')
+            ->line('Pour activer votre compte, veuillez cliquer sur le bouton ci-dessous')
+            ->action('Activer mon compte', $this->verificationUrl($notifiable));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
